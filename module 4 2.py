eec0c8722a965d301057a685079b9d03178b377a
MrAlM <81728811+ALMAZIK17364@users.noreply.github.com>
2024-08-09 19:08:56 +0800

    Add files via upload
    2023/10/20 00:00|Домашняя работа по уроку "Пространство имен."

---
+++ module 4 2.py
@@ -0,0 +1,8 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+test_function()
+
+# inner_function() - не получится вызвать вне функции test_function
